365dfa10090774e64e2201eae6c7543bfaafad67
Christie <stephenson.mchristine@gmail.com>
2014-05-19 09:28:51 -0600

    Removing files we don't care about

--- public/js/constants.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-/* Constants */
-
-angular.module('sproutApp.constants', [])
-    .constant('version', '0.1')
-    .constant('baseTitle', 'Recipe Organizer Project | ')
-    .constant('brand', 'Recipe Organizer');
